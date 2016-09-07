7ce9c9b152e968ebf58cb03f3c98215549f6ed57
Alejandro Caballero <lava.caballero@gmail.com>
2016-09-07 02:56:02 -0500

    Added header button identifiers

--- admin.php
+++ admin.php
@@ -104,7 +104,7 @@ header("Content-Type: text/html; charset=utf-8"); ?>
                 </span>
             <? endif; ?>
             
-            <a class="main_menu_item always_visible pull-left" href="<?= $config->full_root_path ?>/">
+            <a id="home_menu_button" class="main_menu_item always_visible pull-left" href="<?= $config->full_root_path ?>/">
                 <span class="fa fa-home fa-fw"></span>
             </a>
             
