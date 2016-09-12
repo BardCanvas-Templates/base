4dff116ef5d4a1cbe1357c3d6e0c12e8dcc26308
Alejandro Caballero <lava.caballero@gmail.com>
2016-09-12 02:29:25 -0500

    CSS tuning

--- media/left_sidebar_addon.js
+++ media/left_sidebar_addon.js
@@ -1,6 +1,10 @@
 
+var window_width_for_left_sidebar = 0;
+
 function toggle_left_sidebar_items()
 {
+    window_width_for_left_sidebar = $(window).width();
+    
     var visible        = $('#left_sidebar').is(':visible');
     var bar_visibility = visible ? 'false' : 'true';
     
@@ -14,7 +18,11 @@ $(document).ready(function()
     
     $(window).resize(function()
     {
-        if( $('body').attr('data-left-sidebar-visible') == 'true' )
-            toggle_left_sidebar_items();
+        if( $('body').attr('data-left-sidebar-visible') != 'true' ) return;
+            
+        var width = $(window).width();
+        if( width == window_width_for_left_sidebar ) return;
+        
+        toggle_left_sidebar_items();
     });
 });
